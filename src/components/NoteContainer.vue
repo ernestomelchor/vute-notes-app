f426404e94d38f706cea739410ab23fd3708a625
ernestomelchor <nurturelove88@gmail.com>
2020-04-21 19:48:02 -0500

    break toolbar editor and notes into components

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note-container">
+    <note-selectors />
+    <note-editor />
+  </div>
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors";
+import NoteEditor from "./NoteEditor";
+export default {
+  name: "note-container",
+  components: {
+    NoteSelectors,
+    NoteEditor
+  }
+};
+</script>
